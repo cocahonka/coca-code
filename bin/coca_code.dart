40b5fbdbb2dc5ddaec44f331d9be7d153203ef9d
cocahonka <cocahonka@gmail.com>
2024-05-31 17:18:27 +0300

    update main

--- bin/coca_code.dart
+++ bin/coca_code.dart
@@ -1,1 +1,27 @@
-void main() {}
+const greetings = [
+  'Hello',
+  'Привет',
+  'Hola',
+  'Bonjour',
+  'Ciao',
+  '안녕하세요',
+  'こんにちは',
+  '你好',
+  'Olá',
+  'Xin chào',
+  'สวัสดี',
+  'Hallå',
+  'Hoi',
+  'Salut',
+  'שלום',
+  'مرحبا',
+  'नमस्ते',
+  'Cześć',
+  'Γειά σας',
+];
+
+void main(List<String> args) {
+  final greeting = ([...greetings]..shuffle()).first;
+
+  print('$greeting, ${args.first}');
+}
