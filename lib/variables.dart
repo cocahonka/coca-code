5a56c0d0bd737f2ebf2a4e905ebb8125ebcbcf64
hashiriya <1nikita05@mail.ru>
2024-05-31 19:28:55 +0300

    умный кабанчик

--- lib/variables.dart
+++ lib/variables.dart
@@ -3,27 +3,27 @@ import 'dart:io';
 
 void variables() {
   // Создание переменных различных типов
-  int age = 25;
-  double height = 1.75;
-  String name = 'aksenoff';
-  bool isStudent = true;
+  const age = 25;
+  const height = 1.75;
+  const name = 'aksenoff';
+  const isStudent = true;
   dynamic variable = 42;
-  Object object = 'SUAI';
+  const Object object = 'SUAI';
 
   // Изменение значения динамической переменной
   variable = 'age multiply by 5 is ${age * 5}';
 
   // Запрос данных у пользователя
   stdout.write('Введите ваше имя: ');
-  String userName = stdin.readLineSync()!;
+  final userName = stdin.readLineSync()!;
 
   stdout.write('Введите ваш возраст: ');
-  String userAgeInput = stdin.readLineSync()!;
-  int userAge = int.parse(userAgeInput);
+  final userAgeInput = stdin.readLineSync()!;
+  final userAge = int.parse(userAgeInput);
 
   stdout.write('Введите ваше любимое дробное число: ');
-  String userFavoriteNumberInput = stdin.readLineSync()!;
-  double userFavoriteNumber = double.parse(userFavoriteNumberInput);
+  final userFavoriteNumberInput = stdin.readLineSync()!;
+  final userFavoriteNumber = double.parse(userFavoriteNumberInput);
 
   // Вывод результатов
   print('Name: $name, Age: $age, Height: $height, Student: $isStudent, Dynamic: $variable, Object: $object');
